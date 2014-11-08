36efdd405e0fa46e2228be41126196b4c19d0c62
Ramon Tayag <ramon.tayag@gmail.com>
2014-11-08 08:53:53 +0800

    Remove bit wallet factories

--- spec/factories.rb
+++ spec/factories.rb
@@ -24,30 +24,4 @@ FactoryGirl.define do
     transaction_id "xx1"
   end
 
-  factory :bit_wallet, :class => OpenStruct do
-  end
-
-  factory :bit_wallet_account, :class => OpenStruct do
-    sequence(:name) {|n| n.to_s}
-    addresses do |account|
-      [FactoryGirl.build(:bit_wallet_address, account: account)]
-    end
-  end
-
-  factory :bit_wallet_address, :class => OpenStruct do
-    account { FactoryGirl.build(:bit_wallet_account) }
-    sequence(:address) {|n| "address_#{n+1000}"}
-  end
-
-  factory :bit_wallet_transaction, :class => OpenStruct do
-    account { FactoryGirl.build(:bit_wallet_account) }
-    sequence(:id) {|n| "longhash_#{n}"}
-    sequence(:address_str) {|n| "address_#{n}"}
-    amount 1.22
-    category 'receive'
-    confirmations 0
-    occurred_at 1.minute.ago
-    received_at 1.minute.ago
-  end
-
 end
